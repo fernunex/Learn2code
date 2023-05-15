878a72accd64bb02bf4b254a9bf2e67b873b88be
Fernando Nuñez <fer.bsof@outlook.com>
2023-05-15 13:18:16 -0600

    First problem solved Word Count

---
+++ 1_getting_started/word_count.py
@@ -0,0 +1,7 @@
+# Problem name: DMOPC '15 Contest 7 P2 - Not a Wall of Text
+# Code: dmopc15c7p2
+# Link: https://dmoj.ca/problem/dmopc15c7p2
+
+line = input("")
+total_words = line.count(' ') + 1
+print(total_words)
