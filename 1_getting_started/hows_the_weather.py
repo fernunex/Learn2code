3f5bc32775f5f5d2e503a4307c4a1acefd1abdbc
Fernando Nuñez <fer.bsof@outlook.com>
2023-05-16 09:49:30 -0600

    How's the weather Problem Solved

---
+++ 1_getting_started/hows_the_weather.py
@@ -0,0 +1,8 @@
+# Problem name: WC '17 Contest 1 J2 - How's the Weather?
+# Code: wc17c1j2
+# Link: https://dmoj.ca/problem/wc17c1j2
+
+celsius_deg = int(input())
+fahrenheit_deg = (celsius_deg * 9/5) + 32
+
+print(fahrenheit_deg)
