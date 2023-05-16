59abadb9a29ad705a20eff2333e452910681c001
Fernando Nuñez <fer.bsof@outlook.com>
2023-05-16 09:34:14 -0600

    New Hope Problem Solved

---
+++ 1_getting_started/new_hope.py
@@ -0,0 +1,8 @@
+# Problem name: WC '15 Contest 2 J1 - A New Hope
+# Code: wc15c2j1
+# Link: https://dmoj.ca/problem/wc15c2j1
+
+far_count = int(input()) - 1
+sentence = 'A long time ago in a galaxy ' + 'far, ' * far_count + 'far away...'
+
+print(sentence)
