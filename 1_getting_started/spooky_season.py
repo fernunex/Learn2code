b111b6d8f0141c5f449a48bf0bcdd4b40e0682b2
Fernando Nuñez <fer.bsof@outlook.com>
2023-05-16 09:22:20 -0600

    Spooky Season Problem solved

---
+++ 1_getting_started/spooky_season.py
@@ -0,0 +1,8 @@
+# Problem name: WC '16 Contest 1 J1 - A Spooky Season
+# Code: wc16c1j1
+# Link: https://dmoj.ca/problem/wc16c1j1
+
+spokiness_level = int(input())
+spoky_string = 'sp' + 'o' * spokiness_level + 'ky'
+
+print(spoky_string)
