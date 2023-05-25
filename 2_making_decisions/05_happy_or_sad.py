dc7af11d7f2d8fc08d37692e85c90250ec3ba71f
Fernando Nuñez <fer.bsof@outlook.com>
2023-05-25 13:21:12 -0600

    Happy or Sad Solved

---
+++ 2_making_decisions/05_happy_or_sad.py
@@ -0,0 +1,17 @@
+# Problem name: 
+# Code: ccc15j2
+# Link: https://dmoj.ca/problem/ccc15j2
+
+message = input()
+
+sad_emoticon = message.count(':-(')
+happy_emoticon = message.count(':-)')
+
+if (sad_emoticon == 0 and happy_emoticon == 0):
+    print('none')
+elif sad_emoticon > happy_emoticon:
+    print('sad')
+elif sad_emoticon < happy_emoticon:
+    print('happy')
+else:
+    print('unsure')
